7ff428ad81d976681b42db81c587c738b1613c71
jyldyzkan <kgkalla7@gmail.com>
2023-10-25 20:01:12 +0600

    first commit

---
+++ JS1/script.js
@@ -0,0 +1,35 @@
+// boolean = true/false
+// undefined
+// string=''=""
+// number=1
+// nul=
+// bigint=
+
+
+// let a = 'hello'
+// let b = ' jyldyz'
+// console.log( a + b)
+
+// let a = 4
+// let b = 55
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// let a = 'privet'
+// let b = 3
+// console.log(a - b)
+
+
+// let a = 5
+// let b = '5'
+// console.log(a + + b)
+
+// let c = 5
+// let a = undefined
+// console.log(c - a)
+
+
+
+
